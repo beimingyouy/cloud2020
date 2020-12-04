eabfbd56750889580cf03ef1a21bdbec37aedca7
zhuangyi <zhuangyi@taqu.cn>
2020-12-04 15:34:46 +0800

    add eureka

---
+++ learn-eureka/src/main/java/com/hl/springcloud/learneureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.hl.springcloud.learneureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: zy
+ * @Date: 2020/12/4 15:33
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
